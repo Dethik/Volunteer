b1b0472ec2d13f4efc7d62c162df7b66717b7c87
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-12 15:34:51 -0800

    start of volunteers incorporation

--- app.rb
+++ app.rb
@@ -66,16 +66,22 @@ post('/projects/:id/volunteers') do
   erb(:project)
 end
 
-patch ('/projects/:id/volunteers/:volunteer_id') do
+patch('/projects/:id/volunteers/:volunteer_id') do
   @project = Project.find(params[:id].to_i())
   volunteer = Volunteer.find(params[:volunteer_id].to_i())
   volunteer.update(params[:name], @project.id)
   erb(:project)
 end
 
-delete ('/projects/:id/volunteers/:volunteer_id') do
+delete('/projects/:id/volunteers/:volunteer_id') do
   volunteer = Volunteer.find(params[:volunteer_id].to_i())
   volunteer.delete
   @project = Project.find(params[:id].to_i())
   erb(:project)
+end
+
+get('/volunteers') do
+  @volunteer = Volunteer.all
+  @project = Project.find(params[:id].to_i())
+  erb(:volunteers)
 end
