0eb0637f7af3b0a59f0a19d719df4d255c80734c
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-12 10:42:03 -0800

    add app.rb logic and views for pages

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ require './lib/volunteer'
 require './lib/project'
 also_reload 'lib/**/*.rb'
 
+get('/') do
+  redirect to('/tracker')
+end
+
+get('/tracker') do
+  erb(:tracker)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
